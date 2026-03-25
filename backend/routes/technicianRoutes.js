e146c8dfac73327e78f579255ea7cfe414b9e037
jmrprasanna <jmrprasanna@gmail.com>
2026-03-25 23:13:11 +0530

    update

--- backend/routes/technicianRoutes.js
+++ backend/routes/technicianRoutes.js
@@ -5,9 +5,9 @@ const authMiddleware = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
 
 router.get("/", authMiddleware, roleMiddleware(["admin", "manager", "user"]), technicianController.getTechnicians);
-router.get("/:id", authMiddleware, roleMiddleware(["admin", "manager"]), technicianController.getTechnicianById);
-router.post("/", authMiddleware, roleMiddleware(["admin", "manager"]), technicianController.createTechnician);
-router.put("/:id", authMiddleware, roleMiddleware(["admin", "manager"]), technicianController.updateTechnician);
-router.delete("/:id", authMiddleware, roleMiddleware(["admin", "manager"]), technicianController.deleteTechnician);
+router.get("/:id", authMiddleware, roleMiddleware(["admin", "manager", "user"]), technicianController.getTechnicianById);
+router.post("/", authMiddleware, roleMiddleware(["admin", "manager", "user"]), technicianController.createTechnician);
+router.put("/:id", authMiddleware, roleMiddleware(["admin", "manager", "user"]), technicianController.updateTechnician);
+router.delete("/:id", authMiddleware, roleMiddleware(["admin", "manager", "user"]), technicianController.deleteTechnician);
 
 module.exports = router;
